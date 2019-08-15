99c5d7aa48e53c792801bb5b8f336afb27387351
Flavien PETITGUYOT <f.petitguyot@gmail.com>
2019-08-15 23:03:04 +0200

    Commit for the retour 2

--- new.py
+++ new.py
@@ -1,1 +1,14 @@
+# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non
+# sapien cursus, consequat ex vitae, finibus odio. Duis elementum
+# venenatis rhoncus. Fusce commodo purus non ex lobortis, ornare fringilla
+# tortor fringilla. Praesent pharetra nisi at dictum auctor. Aliquam erat
+# volutpat. Donec vel posuere dui. Sed tempus sodales elit vitae volutpat.
+# Sed non eleifend turpis. Morbi fermentum orci augue, eget accumsan
+# turpis tempor nec. Ut in nibh non tellus pharetra ultrices. Ut diam
+# augue, tincidunt vel rutrum ac, pretium vitae quam. Quisque id risus
+# pulvinar sem euismod aliquet id ac neque. Vestibulum ante ipsum primis
+# in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris
+# viverra nibh egestas, ultricies mi eget, ullamcorper arcu. Morbi
+# facilisis ex elit, quis molestie risus consequat quis.
+
 print('Hello world, again :D.')

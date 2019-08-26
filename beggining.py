0a0934a94db153be093b844fab2da44165fb64e4
Flavien PETITGUYOT <f.petitguyot@gmail.com>
2019-08-26 19:04:53 +0200

    Add summary. (Issue: #6)

--- beggining.py
+++ beggining.py
@@ -2,3 +2,11 @@
 # 2005 ‧ Drame/Cinéma de fantasy ‧ 2h 20m
 # Réalisateur : Christopher Nolan
 # Box-office : 375,2 millions USD
+
+# Comment un homme seul peut-il changer le monde ? Telle est la question
+# qui hante Bruce Wayne depuis cette nuit tragique où ses parents furent
+# abattus sous ses yeux, dans une ruelle de Gotham City. Torturé par un
+# profond sentiment de colère et de culpabilité, le jeune héritier de
+# cette richissime famille fuit Gotham pour un long et discret voyage à
+# travers le monde. Le but de ses pérégrinations : sublimer sa soif de
+# vengeance en trouvant de nouveaux moyens de lutter contre l'injustice.
